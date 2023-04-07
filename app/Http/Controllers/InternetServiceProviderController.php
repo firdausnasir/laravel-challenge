f17b38df737d490d0a43a5cbfc0a5f953aa2a144
firdausnasir <firdaus.nasir@involve.asia>
2023-04-07 15:23:34 +0800

    refactor wifi calculator to use SOLID principle

--- app/Http/Controllers/InternetServiceProviderController.php
+++ app/Http/Controllers/InternetServiceProviderController.php
@@ -2,31 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\InternetServiceProvider\Mpt;
-use App\Services\InternetServiceProvider\Ooredoo;
+use App\Interfaces\InternetServiceProviderInterface;
 use Illuminate\Http\Request;
 
 class InternetServiceProviderController extends Controller
 {
-    public function getMptInvoiceAmount(Request $request)
+    public function getInvoiceAmount(Request $request, InternetServiceProviderInterface $internetServiceProvider)
     {
-        $mpt = new Mpt();
-        $mpt->setMonth($request->get('month') ?: 1);
-        $amount = $mpt->calculateTotalAmount();
-        
+        $month = $request->input('month', 1);
+
+        $internetServiceProvider->setMonth($month);
+
         return response()->json([
-            'data' => $amount
-        ]);
-    }
-    
-    public function getOoredooInvoiceAmount(Request $request)
-    {
-        $ooredoo = new Ooredoo();
-        $ooredoo->setMonth($request->get('month') ?: 1);
-        $amount = $ooredoo->calculateTotalAmount();
-        
-        return response()->json([
-            'data' => $amount
+            'data' => $internetServiceProvider->calculateTotalAmount(),
         ]);
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\InternetServiceProviderInterface;
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            InternetServiceProviderInterface::class,
+            'App\Services\InternetServiceProvider\\'.ucfirst(Request::capture()->segment(2))
+        );
     }
 
     /**
